cf224eec71ee90b1cc4e524e6373061dcf3879cd
Jonathan LEGRAND <jonathan.legrand@ens-lyon.fr>
2025-10-07 00:17:54 +0200

    Update Dockerfile and Entrypoint Script
    - Refactor Dockerfile to use correct user environment variables
    - Update virtual environment setup in Dockerfile
    - Enhance cleanup steps in Dockerfile
    - Modify `entrypoint.sh` to use new environment configuration

--- docker/webui/entrypoint.sh
+++ docker/webui/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Activate the virtual environment
-source /home/${USER_NAME}/venv/bin/activate
+source /home/romi/venv/bin/activate
 # Start the WebUI App
-exec uwsgi --http :${SERVER_PORT} --module plantimager.webui.wsgi:application --callable application --master
+#exec uwsgi --http :${SERVER_PORT:-8080} --module plantimager.webui.wsgi:application --callable application --master --processes=4 --threads=2 --buffer-size=32768
+# Execute whatever command was passed to the container
+/bin/bash -c "$@"
