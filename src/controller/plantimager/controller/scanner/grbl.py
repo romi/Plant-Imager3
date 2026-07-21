10641a5d80a87e915b59e162b71d10c1d144dd16
Arthur Luciani <arthur.luciani@ens-lyon.fr>
2026-07-21 14:53:36 +0200

    Update Z-axis max travel offset and add connection delay in GRBL
    - Changed Z-axis max travel offset for `$132` in `grbl.py` from `360 - 16` to `360 - 52` degrees to accommodate a new encoder offset.
    - Added a 2-second delay after establishing a serial connection to improve GRBL initialization stability.

--- src/controller/plantimager/controller/scanner/grbl.py
+++ src/controller/plantimager/controller/scanner/grbl.py
@@ -80,7 +80,7 @@ GRBL_SETTINGS = {
     "$122": ("Z Acceleration", "deg/sec^2", 50),
     "$130": ("X Max travel", "mm", 740),
     "$131": ("Y Max travel", "mm", 740),
-    "$132": ("Z Max travel", "deg", 360 - 16)  # 16 degree offset from encoder 0
+    "$132": ("Z Max travel", "deg", 360 - 52)  # 52 degree offset from encoder 0
 }
 
 def angle_min_travel(current_angle: deg, desired_angle: deg) -> deg:
@@ -288,6 +288,7 @@ class CNC(AbstractCNC):
                 except serial.SerialException:
                     logger.debug(f"Could not connect to '{port.device}'")
                     continue
+                time.sleep(2)
                 response = s.read_all()
                 logger.debug(f"GRBL response '{response}'")
                 if "\r\nGrbl 1.1h" in response.decode():
